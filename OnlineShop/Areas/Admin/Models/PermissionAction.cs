d68b38307b5b69a2356f0885ab2f6cec06942a61
longdq <longdq2902@gmail.com>
2017-07-06 23:01:25 +0700

    Commit PermissionAction class

---
+++ OnlineShop/Areas/Admin/Models/PermissionAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Areas.Admin.Models
+{
+    public class PermissionAction
+    {
+        public long PermissionId { get; set; }
+        public string PermissionName { get; set; }
+        public string Description { get; set; }
+        public bool isGrant { get; set; }
+    }
+}
